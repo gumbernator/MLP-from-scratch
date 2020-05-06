56ce9c01da54ed8f137d97e39cd22a146bee5e0b
Guyugmonkh Lkhagvachuluun <44472991+gumbernator@users.noreply.github.com>
2020-05-06 22:52:37 +0800

    Delete optimizers.py

--- gumbernator/optimizers.py
+++
@@ -1,4 +0,0 @@
-from .optimizer_implementations.sgd import SGD
-from .optimizer_implementations.adagrad import Adagrad
-from .optimizer_implementations.adadelta import Adadelta
-from .optimizer_implementations.rmsprop import RMSprop
